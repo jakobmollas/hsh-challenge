d98dabbe58191adf08ab808cdae7c3c26c383fdd
Jakob Möllås <jakob.mollas@gmail.com>
2022-12-17 17:34:15 +0100

    Add test project

--- src/WeaponsWatcher/WeaponsFileMonitor.cs
+++ src/WeaponsWatcher/WeaponsFileMonitor.cs
@@ -2,112 +2,111 @@
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
-namespace WeaponsWatcher
+namespace WeaponsWatcher;
+
+// A different approach to using a PeriodicTimer (which is async) could be to use Dispatcher.BeginInvoke and run short diff checks on the target file.
+// That would simplify the solution somewhat (no need to marshall calls to the UI thread, no need for separate threads) but it could also remove some separation of concerns.
+// I am also not sure how to schedule an update to not run immediately, but in x milliseconds or similar.
+
+internal sealed class WeaponsFileMonitor : IAsyncDisposable
 {
-	// A different approach to using a PeriodicTimer (which is async) could be to use Dispatcher.BeginInvoke and run short diff checks on the target file.
-	// That would simplify the solution somewhat (no need to marshall calls to the UI thread, no need for separate threads) but it could also remove some separation of concerns.
-	// I am also not sure how to schedule an update to not run immediately, but in x milliseconds or similar.
+	private static readonly JsonSerializerOptions _jsonOptions = new() { Converters = { new JsonStringEnumConverter() } };
 
-	internal sealed class WeaponsFileMonitor : IAsyncDisposable
+	private readonly CancellationTokenSource _cts = new();
+	private readonly Task _monitorTask;
+	private readonly string _pathToMonitor;
+	private readonly PeriodicTimer _timer;
+	private DateTime _lastWriteTime;
+
+	public event EventHandler<IEnumerable<Weapon>>? WeaponsUpdated;
+
+	/// <summary>
+	/// Create a new <see cref="WeaponsFileMonitor"/> that continuously monitors <paramref name="pathToMonitor"/> for changes (using LastWriteTimeUtc).
+	/// When the file is modified, the <see cref="WeaponsUpdated"/> event will be fired.
+	/// Dispose the instance to terminate the monitoring operation.
+	/// </summary>
+	/// <remarks>
+	/// Cancellation/dispose could result in <see cref="OperationCanceledException"/>.
+	/// </remarks>
+	public WeaponsFileMonitor(string pathToMonitor)
 	{
-		private static readonly JsonSerializerOptions JsonOptions = new() { Converters = { new JsonStringEnumConverter() } };
+		if (string.IsNullOrEmpty(pathToMonitor))
+			throw new ArgumentException("Value cannot be null or empty.", nameof(pathToMonitor));
 
-		private readonly CancellationTokenSource _cts = new();
-		private readonly Task _monitorTask;
-		private readonly string _pathToMonitor;
-		private readonly PeriodicTimer _timer;
-		private DateTime _lastWriteTime;
+		_pathToMonitor = pathToMonitor;
 
-		public event EventHandler<IEnumerable<Weapon>>? WeaponsUpdated;
+		_monitorTask = Task.Run(async () => await RunAsync(_cts.Token));
+		_timer = new PeriodicTimer(TimeSpan.FromMilliseconds(100));
+	}
 
-		/// <summary>
-		/// Create a new <see cref="WeaponsFileMonitor"/> that continuously monitors <paramref name="pathToMonitor"/> for changes (using LastWriteTimeUtc).
-		/// When the file is modified, the <see cref="WeaponsUpdated"/> event will be fired.
-		/// Dispose the instance to terminate the monitoring operation.
-		/// </summary>
-		/// <remarks>
-		/// Cancellation/dispose could result in <see cref="OperationCanceledException"/>.
-		/// </remarks>
-		public WeaponsFileMonitor(string pathToMonitor)
+	public async ValueTask DisposeAsync()
+	{
+		_cts.Cancel();
+
+		// This could 
+		await _monitorTask;
+	}
+
+	private async Task RunAsync(CancellationToken ct)
+	{
+		while (!ct.IsCancellationRequested)
 		{
-			if (string.IsNullOrEmpty(pathToMonitor))
-				throw new ArgumentException("Value cannot be null or empty.", nameof(pathToMonitor));
+			// Dispose/token cancel does not result in an OperationCanceledException
+			// so we do not need to do a try/catch
+			bool result = await _timer.WaitForNextTickAsync(ct);
+			if (!result)
+				return;
 
-			_pathToMonitor = pathToMonitor;
-
-			_monitorTask = Task.Run(async () => await RunAsync(_cts.Token));
-			_timer = new PeriodicTimer(TimeSpan.FromMilliseconds(100));
+			await CheckForUpdatesAsync(ct);
 		}
+	}
 
-		public async ValueTask DisposeAsync()
+	private async Task CheckForUpdatesAsync(CancellationToken ct)
+	{
+		try
 		{
-			_cts.Cancel();
+			// No listeners or nothing to read? Well, no point in working hard in that case.
+			// By checking for subscribers we also handle an edge case where a consumer creates a monitor
+			// that starts and processes a file BEFORE the consumer has time to subscribe to the change event,
+			// which would lead to a lost initial update.
+			if (WeaponsUpdated == null || !File.Exists(_pathToMonitor))
+				return;
 
-			// This could 
-			await _monitorTask;
+			var writeTime = File.GetLastWriteTimeUtc(_pathToMonitor);
+			if (writeTime == _lastWriteTime)
+				return;
+
+			_lastWriteTime = writeTime;
+
+			var weapons = await ReadWeaponsAsync(ct);
+			WeaponsUpdated?.Invoke(this, weapons);
 		}
-
-		private async Task RunAsync(CancellationToken ct)
+		catch (Exception)
 		{
-			while (!ct.IsCancellationRequested)
-			{
-				// Dispose/token cancel does not result in an OperationCanceledException
-				// so we do not need to do a try/catch
-				var result = await _timer.WaitForNextTickAsync(ct);
-				if (!result)
-					return;
-
-				await CheckForUpdatesAsync(ct);
-			}
+			// Ignore for now - in a real app we would probably like to log this, depending on what is expected.
 		}
+	}
 
-		private async Task CheckForUpdatesAsync(CancellationToken ct)
+	/// <summary>
+	/// Do a single read, handle errors, does not catch <see cref="OperationCanceledException"/>.
+	/// </summary>
+	/// <returns>An empty array in case of any errors (file-related errors are mainly expected here).</returns>
+	private async Task<IReadOnlyList<Weapon>> ReadWeaponsAsync(CancellationToken ct)
+	{
+		try
 		{
-			try
-			{
-				// No listeners or nothing to read? Well, no point in working hard in that case.
-				// By checking for subscribers we also handle an edge case where a consumer creates a monitor
-				// that starts and processes a file BEFORE the consumer has time to subscribe to the change event,
-				// which would lead to a lost initial update.
-				if (WeaponsUpdated == null || !File.Exists(_pathToMonitor))
-					return;
-
-				var writeTime = File.GetLastWriteTimeUtc(_pathToMonitor);
-				if (writeTime == _lastWriteTime)
-					return;
-
-				_lastWriteTime = writeTime;
-
-				var weapons = await ReadWeaponsAsync(ct);
-				WeaponsUpdated?.Invoke(this, weapons);
-			}
-			catch (Exception)
-			{
-				// Ignore for now - in a real app we would probably like to log this, depending on what is expected.
-			}
-		}
-
-		/// <summary>
-		/// Do a single read, handle errors, does not catch <see cref="OperationCanceledException"/>.
-		/// </summary>
-		/// <returns>An empty array in case of any errors (file-related errors are mainly expected here).</returns>
-		private async Task<IReadOnlyList<Weapon>> ReadWeaponsAsync(CancellationToken ct)
-		{
-			try
-			{
-				if (!File.Exists(_pathToMonitor))
-					return Array.Empty<Weapon>();
-
-				await using var readStream = File.OpenRead(_pathToMonitor);
-				var weapons = await JsonSerializer.DeserializeAsync<List<Weapon>>(readStream, JsonOptions, ct);
-
-				return weapons != null ? weapons : Array.Empty<Weapon>();
-			}
-			catch (Exception)
-			{
-				// In a real application we would probably want to log this, but in this example we simply clear out the data/UI.
+			if (!File.Exists(_pathToMonitor))
 				return Array.Empty<Weapon>();
-			}
+
+			await using var readStream = File.OpenRead(_pathToMonitor);
+			var weapons = await JsonSerializer.DeserializeAsync<List<Weapon>>(readStream, _jsonOptions, ct);
+
+			return weapons != null ? weapons : Array.Empty<Weapon>();
+		}
+		catch (Exception)
+		{
+			// In a real application we would probably want to log this, but in this example we simply clear out the data/UI.
+			return Array.Empty<Weapon>();
 		}
 	}
 }
---
+++ test/WeaponsWatcher.Tests/WeaponsViewModelTests.cs
@@ -0,0 +1,49 @@
+﻿using KellermanSoftware.CompareNetObjects;
+using Xunit;
+
+namespace WeaponsWatcher.Tests;
+
+public class WeaponsViewModelTests
+{
+	[Fact]
+	public void Update_Clears_And_Updates_Collection()
+	{
+		// Arrange
+		var viewModel = new WeaponsViewModel();
+		Assert.Empty(viewModel.Weapons);
+
+		IEnumerable<Weapon> newWeapons = CreateWeapons();
+
+		// Act
+		viewModel.UpdateWeapons(newWeapons);
+
+		// Assert
+		viewModel.Weapons.ShouldCompare(newWeapons, compareConfig: new ComparisonConfig { IgnoreObjectTypes = true });
+	}
+
+	[Fact]
+	public void Clears_Empties_Collection()
+	{
+		// Arrange
+		var viewModel = new WeaponsViewModel();
+
+		var newWeapons = CreateWeapons();
+		viewModel.UpdateWeapons(newWeapons);
+
+		// Act
+		viewModel.ClearWeapons();
+
+		// Assert
+		Assert.Empty(viewModel.Weapons);
+	}
+
+	private static List<Weapon> CreateWeapons()
+	{
+		return new List<Weapon>
+		{
+			new("Boomerang", TechType.Power, 0.001),
+			new("ÚSB-drive", TechType.Tech, 4.4),
+			new("Rubik Cube", TechType.Smart, 12.55)
+		};
+	}
+}
