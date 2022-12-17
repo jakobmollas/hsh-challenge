b087f46c3076ea27ce8a417662e4816478075ea6
Jakob Möllås <jakob.mollas@gmail.com>
2022-12-17 17:44:11 +0100

    Add more tests

---
+++ test/WeaponsWatcher.Tests/WeaponsFileMonitorTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace WeaponsWatcher.Tests;
+
+public class WeaponsViewModelTests
+{
+	[Fact]
+	public void Monitor_With_Bad_Params_Throws()
+	{
+		Assert.Throws<ArgumentException>("pathToMonitor", () => new WeaponsFileMonitor(null!, TimeSpan.Zero));
+		Assert.Throws<ArgumentException>("pathToMonitor", () => new WeaponsFileMonitor("", TimeSpan.Zero));
+		Assert.Throws<ArgumentException>("pathToMonitor", () => new WeaponsFileMonitor("   ", TimeSpan.Zero));
+
+		Assert.Throws<ArgumentOutOfRangeException>("interval", () => new WeaponsFileMonitor("test", TimeSpan.Zero));
+		Assert.Throws<ArgumentOutOfRangeException>("interval", () => new WeaponsFileMonitor("test", TimeSpan.FromSeconds(-1)));
+	}
+}
