b77ab04a980a868b819cbd809dda8ead6efe2e68
Jakob Möllås <jakob.mollas@gmail.com>
2022-12-18 00:51:01 +0100

    Refactor

--- src/WeaponsWatcher/MainWindow.xaml.cs
+++ src/WeaponsWatcher/MainWindow.xaml.cs
@@ -19,18 +19,18 @@ public partial class MainWindow
 
 		InitializeComponent();
 
-		Closed += MainWindow_Closed;
-
 		string pathToMonitor = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, Filename);
 		_monitor = new WeaponsFileMonitor(pathToMonitor, TimeSpan.FromMilliseconds(250));
 		_monitor.WeaponsUpdated += _monitor_WeaponsUpdated;
+
+		Closed += MainWindow_Closed;
 	}
 
 	private async void MainWindow_Closed(object? sender, EventArgs e)
 	{
-		// I suspect there are better ways to do async shutdown
 		try
 		{
+			// I suspect there are better ways to do async shutdown
 			await _monitor.DisposeAsync();
 		}
 		catch (OperationCanceledException)
@@ -46,8 +46,9 @@ public partial class MainWindow
 
 	private void _monitor_WeaponsUpdated(object? sender, IEnumerable<Weapon> weapons)
 	{
-		// There is an obvious corner case here, what if the change event is triggered faster than the consumer?
-		// Monitoring files is a tricky business - we could add some hysteresis where we wait a short while before triggering read, for example wait until a second has passed with no new events.
+		// There is an obvious corner case here, what if the change event is triggered faster than the consuming code can process them?
+		// We could flood the UI with updates in that case.
+		// Monitoring files is tricky business - we could add some hysteresis where we wait a short while before triggering read, for example wait until a second has passed with no new events.
 		// And a queue on top of that to handle events that pop in during an ongoing read - we do not want to miss updates just because we are currently reading the file.
 		// Doing that would introduce more corner cases and room for optimizations etc.
 		Dispatcher.Invoke(() => ViewModel.UpdateWeapons(weapons));
